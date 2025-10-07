c2bf8ced04d13228df3ebaa7ee67433cc0874d98
Hallelujah Kebede <matiashalleluya19@gmail.com>
2025-10-07 13:39:46 +0300

    Chat and requests working code

--- lib/config.dart
+++ lib/config.dart
@@ -1,2 +1,2 @@
-const cloudRunHost = 'https://paloni-webapp-926930663141.us-central1.run.app';
+const cloudRunHost = 'paloni-webapp-926930663141.us-central1.run.app';
 const model = 'gemini-2.5-pro';
--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,10 @@
+import 'package:uuid/uuid.dart';
 import 'config.dart';
 import 'firebase_options.dart';
 import 'dart:convert';
-import 'dart:math';
 
-import 'package:firebase_ai/firebase_ai.dart';
 import 'package:http/http.dart' as http;
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter_ai_toolkit/flutter_ai_toolkit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_chat_core/flutter_chat_core.dart';
 import 'package:flutter_chat_ui/flutter_chat_ui.dart';
@@ -30,6 +28,9 @@ class ChatPage extends StatefulWidget {
 
 class ChatPageState extends State<ChatPage> {
   final _chatController = InMemoryChatController();
+  var userID = Uuid().v4();
+  var client = http.Client();
+
 
   @override
   void dispose() {
@@ -38,45 +39,49 @@ class ChatPageState extends State<ChatPage> {
   }
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context){
     return Scaffold(
       body: Chat(
         chatController: _chatController,
-        currentUserId: 'user1',
+        currentUserId: userID,
         onMessageSend: (text) {
           _chatController.insertMessage(
             TextMessage(
-              // Better to use UUID or similar for the ID - IDs must be unique
-              id: '${Random().nextInt(1000) + 1}',
-              authorId: 'user1',
+              id: Uuid().v1(),
+              authorId: userID,
               createdAt: DateTime.now().toUtc(),
               text: text,
             ),
           );
 
-          var messagesSize = _chatController.messages.length - 1;
-          var lastMessage = _chatController.messages[messagesSize];
-          var lastText = lastMessage.toJson()['text'];
-          debugPrint("Query: $lastText");
+          int messagesSize = _chatController.messages.length - 1;
+          Message lastMessage = _chatController.messages[messagesSize];
+          String lastText = lastMessage.toJson()['text'];
 
-          String response = "No response";
           try
           {
-            var getresponse = queryEndpoints('ask_gemini', lastText).then((text) {response = text;});
+            var request = askGemini(lastText);
+            request.then((responseBody){
+              var response = jsonDecode(responseBody)['output'];
+
+              _chatController.insertMessage(
+              TextMessage(
+                id: Uuid().v1(),
+                authorId: "Gemini",
+                createdAt: DateTime.now().toUtc(),
+                text: response,
+              ));});
           }
           catch(e)
           {
-              debugPrint(e.toString());
+              _chatController.insertMessage(
+              TextMessage(
+                id: Uuid().v1(),
+                authorId: "Gemini",
+                createdAt: DateTime.now().toUtc(),
+                text: "Gemini not responding",
+              ));
           }
-
-          _chatController.insertMessage(
-            TextMessage( // Better to use UUID or similar for the ID - IDs must be unique
-              id: '${Random().nextInt(1000) + 1}',
-              authorId: 'Gemini',
-              createdAt: DateTime.now().toUtc(),
-              text: response,
-              )
-          );
         },
         resolveUser: (UserID id) async {
           return User(id: id, name: 'John Doe');
@@ -85,18 +90,12 @@ class ChatPageState extends State<ChatPage> {
     );
   }
 
-  static Future<String> queryEndpoints(String endpointName, String prompt) async 
+  Future<String> askGemini(String prompt) async
   {
-    Uri endpoint = Uri.https(cloudRunHost, "/$endpointName", {"query" :prompt});
-    var response = await http.get(endpoint);
+    Uri endpoint = Uri.parse('$cloudRunHost/ask_gemini?query=$prompt');
 
-    if(response.statusCode == 200)
-    {
-        var result = response.body;
-        return result;
-    } 
-
-    return response.statusCode.toString();
+    var response = await client.get(endpoint, headers: {'Client': userID,'Accept': '*/*', 'Accept-Encoding': 'gzip, deflate, br'});
+    return response.body;
   }
 }
 
@@ -118,79 +117,7 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class GeminiChatPage extends StatelessWidget {
-  const GeminiChatPage({super.key});
- 
-  @override
-  Widget build(BuildContext context) => Scaffold(
-    appBar: AppBar(title: const Text(MyApp.title)),
-    body: LlmChatView(
-      provider: FirebaseProvider( // use FirebaseProvider and vertexAI()
-        model: FirebaseAI.googleAI().generativeModel(
-          model: model, 
-          systemInstruction: Content.text('You are a kind and funny commercial conversational agent.'),
-          // tools: [
-          //   Tool.functionDeclarations([getDeclaration('queryDatabaseGemini', 'Pass query to gemini model')])
-          // ]
-          ),
-      ),
-    ),
-  );
 
-  
-  static  FunctionDeclaration getDeclaration(String name, String description){
-    var names = ['text', 'attachments'];
-    var schemas = [Schema.string(), Schema.array(items:Schema.integer())];
-    Schema schema = Schema.object(properties: Map<String, Schema>.fromIterables(names, schemas));
-    
-    return FunctionDeclaration(
-            name,
-            description,
-            parameters: Map<String, Schema>.fromIterables(['prompt'], [schema])
-          );
-  }
-
-  static Future<String> queryEndpoints(String endpointName, String prompt) async 
-  {
-    Uri endpoint = Uri.https(cloudRunHost, "/$endpointName", {"text": prompt});
-    var response = await http.get(endpoint);
-
-    if(response.statusCode == 200)
-    {
-        debugPrint("gemini response:${response.body}");
-        var result = response.body;
-        return result;
-    } 
-
-    return response.statusCode.toString();
-  }
-
-  static Future<String> queryDatabaseGemini(String prompt) async 
-  {
-    debugPrint("gemini request:${prompt}");
-    return queryEndpoints('search_database', prompt);
-  }
-}
-
-class Prompt {
-  String? text;
-  Iterable<FileAttachment> attachments = Iterable.empty();
-  
-  Prompt(String text, Iterable<FileAttachment> attachments)
-  {
-    this.text = text;
-    this.attachments = attachments;
-  }
-
-  @override
-  String toString()
-  {
-    var attachmentsBytes = attachments.map((a)=> a.bytes);
-    var attachmentString = attachmentsBytes.join(" ");
-
-    return "$text $attachmentString";
-  }
-}    
 
 
 
