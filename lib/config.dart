b0a8caec7b8cb5a551f422d0301c000221048f2a
hallelujah-101 <hallelujahapplications@gmail.com>
2025-10-23 15:02:46 +0300

    Remove storeLink constant from config

--- lib/config.dart
+++ lib/config.dart
@@ -2,4 +2,3 @@ const cloudRunHost = 'https://paloni-webapp-d5a7dvfvta-uc.a.run.app';
 const model = 'gemini-2.5-pro';
 const databaseId = 'Users';
 const collectionName = 'CHAT_HISTORY';
-const storeLink = 'https://www.childrensplace.com/us/home';
